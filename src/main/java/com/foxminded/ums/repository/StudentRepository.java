7a0bf2ec82064ff8a72067d8c3667f76bc27eea3
Max11mus <koseymax@gmail.com>
2022-08-05 12:40:59 +0300

    Set   TimeZone to Etc/UTC" In timeTableUnitRepository used LocalDateTime parameters instead of ints. Student and Teacher now inherited from Person abstract class Add pagination for StudentRepository and StudentRepository DTO classes refasctored

--- src/main/java/com/foxminded/ums/repository/StudentRepository.java
+++ src/main/java/com/foxminded/ums/repository/StudentRepository.java
@@ -2,11 +2,12 @@ package com.foxminded.ums.repository;
 
 import com.foxminded.ums.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, UUID>,
-        extends PagingAndSortingRepository<Student, UUID>{
+        PagingAndSortingRepository<Student, UUID> {
         }
