976d2f773bcf3a3b667feea173f172d0f59913ee
DESKTOP-URPJ43V\Max11mus <koseymax@gmail.com>
2022-08-23 05:11:41 +0300

    Test added

---
+++ src/test/java/com/foxminded/ums/controllers/LectureRestControllerTest.java
@@ -0,0 +1,12 @@
+package com.foxminded.ums.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LectureRestControllerTest {
+
+    @Test
+    void findStudent() {
+    }
+}
---
+++ src/test/java/com/foxminded/ums/controllers/TimeTableRestControllerTest.java
@@ -0,0 +1,24 @@
+package com.foxminded.ums.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeTableRestControllerTest {
+
+    @Test
+    void getDailyTimeTableForStudent() {
+    }
+
+    @Test
+    void getMonthlyTimeTableForStudent() {
+    }
+
+    @Test
+    void getDailyTimeTableForTeacher() {
+    }
+
+    @Test
+    void getMonthyTimeTableForTeacher() {
+    }
+}
