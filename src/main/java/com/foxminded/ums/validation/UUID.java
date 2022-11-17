17459556acf131ac14ccbaf17b2ffb626fdf68b0
DESKTOP-URPJ43V\Max11mus <koseymax@gmail.com>
2022-11-17 12:57:25 +0200

    Static imports with * had removed in com.foxminded.ums.validation

--- src/main/java/com/foxminded/ums/validation/UUID.java
+++ src/main/java/com/foxminded/ums/validation/UUID.java
@@ -6,7 +6,10 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target({FIELD, METHOD, PARAMETER, ANNOTATION_TYPE})
@@ -14,8 +17,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Constraint(validatedBy = UUIDValidator.class)
 @Documented
 public @interface UUID {
-    String message() default "${validatedValue}  isn't correct " +
-            " UUID. See RFC 4122 - 4.1. Format";
+    String message() default "${validatedValue}  isn't correct " + " UUID. See RFC 4122 - 4.1. Format";
 
     Class<?>[] groups() default {};
 
