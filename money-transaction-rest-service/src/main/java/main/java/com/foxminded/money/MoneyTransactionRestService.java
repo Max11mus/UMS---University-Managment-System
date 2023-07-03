2216623e69f8b9e72f0cdeda319c18a43eaa6714
max11mus <koseymax@gmail.com>
2023-07-03 14:34:09 +0300

    Moved to Java 11 Added Actuator for health monitoring Inject environment variables into spring properties file

--- money-transaction-rest-service/src/main/java/main/java/com/foxminded/money/MoneyTransactionRestService.java
+++ money-transaction-rest-service/src/main/java/main/java/com/foxminded/money/MoneyTransactionRestService.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 import java.util.TimeZone;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class MoneyTransactionRestService {
 
 	public static void main(String[] args) {
